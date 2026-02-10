e460a724e0b06e13d8eaf6707cadc148ad0b1f66
adlamas <alan12394@hotmail.com>
2026-02-10 00:20:03 -0300

    Create CREATE todo list method

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -8,11 +8,8 @@ module Api
     end
 
     def create
+      
       render json: { ruta: 'Entro en el metodo CREATE' }
-    end
-
-    def coleccion
-      render json: { ruta: 'Entro en el metodo coleccion' }
     end
   end
 end
