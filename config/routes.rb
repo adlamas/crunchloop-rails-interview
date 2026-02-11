c11e70973984c4f7973d12d3c9fb175b9a0a0445
adlamas <alan12394@hotmail.com>
2026-02-11 12:26:36 -0300

    Delete unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
       resources :todo_items, only: %i[index create destroy update] do
         put 'complete'
       end
-
-      resources :notes, only: %i[index create], module: :todo_lists
     end
   end
 
